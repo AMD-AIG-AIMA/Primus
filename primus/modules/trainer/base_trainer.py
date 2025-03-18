f8521b39c5ba437b04729a45d06f77e9ff31b2d8
wenxie-amd <Wen.Xie@amd.com>
2025-03-18 10:31:53 +0800

    align the deepseek v3 training parameters (#7)

--- primus/modules/trainer/base_trainer.py
+++ primus/modules/trainer/base_trainer.py
@@ -11,17 +11,14 @@ from megatron.core.models.gpt import GPTModel
 
 
 class BaseTrainer(ABC):
-    # def get_batch_func(self):
     @abstractmethod
     def get_batch(self, data_iterator):
-        raise NotImplementedError
+        pass
 
-    # def get_loss_func(self):
     @abstractmethod
     def loss_func(self, loss_mask: torch.Tensor, output_tensor: torch.Tensor):
-        raise NotImplementedError
+        pass
 
-    # def get_forward_step_func(self):
     @abstractmethod
     def forward_step(self, data_iterator, model: GPTModel):
-        raise NotImplementedError
+        pass
