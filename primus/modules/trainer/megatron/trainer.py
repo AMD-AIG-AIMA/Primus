15376a43285a0544d8e51a4cd059f8ecbc8ff8c9
wenxie-amd <Wen.Xie@amd.com>
2025-03-18 10:38:28 +0800

    copyright (#8)

--- primus/modules/trainer/megatron/trainer.py
+++ primus/modules/trainer/megatron/trainer.py
@@ -1,3 +1,10 @@
+###############################################################################
+# Copyright (c) 2025, Advanced Micro Devices, Inc. All rights reserved.
+#
+# See LICENSE for license information.
+#################################################################################
+
+
 import dataclasses
 import os
 import time
