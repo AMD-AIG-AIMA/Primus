9e73a9f34a56178a189a695f59bdc5182de85433
wenxie-amd <wen.xie@amd.com>
2025-03-18 02:37:40 +0000

    add copyright

--- primus/platform/base_platform.py
+++ primus/platform/base_platform.py
@@ -1,3 +1,9 @@
+###############################################################################
+# Copyright (c) 2025, Advanced Micro Devices, Inc. All rights reserved.
+#
+# See LICENSE for license information.
+#################################################################################
+
 import os
 import socket
 from abc import ABC, abstractmethod
