c027b939668e9badc82049d659c673a791f50334
lihuan <lihuzhan@amd.com>
2025-05-29 20:30:27 +0800

    fix(Megatron): fix interleaved virtual pipeline training error

--- primus/modules/trainer/megatron/trainer.py
+++ primus/modules/trainer/megatron/trainer.py
@@ -10,8 +10,6 @@ import os
 import statistics
 import sys
 import time
-from contextlib import nullcontext
-from typing import Union
 
 import megatron
 import torch
@@ -51,12 +49,6 @@ from megatron.core.datasets.gpt_dataset import (
 )
 from megatron.core.datasets.utils import get_blend_from_list
 from megatron.core.enums import ModelType
-from megatron.core.models.gpt import GPTModel
-from megatron.core.models.gpt.gpt_layer_specs import (
-    get_gpt_decoder_block_spec,
-    get_gpt_layer_local_spec,
-    get_gpt_layer_with_transformer_engine_spec,
-)
 from megatron.core.num_microbatches_calculator import (
     get_current_global_batch_size,
     get_current_running_global_batch_size,
@@ -73,7 +65,6 @@ from megatron.core.rerun_state_machine import (
     initialize_rerun_state_machine,
 )
 from megatron.core.transformer.moe.moe_utils import track_moe_metrics
-from megatron.core.transformer.spec_utils import import_module
 from megatron.core.utils import check_param_hashes_across_dp_replicas, get_model_config
 from megatron.training import (
     ft_integration,
@@ -84,7 +75,6 @@ from megatron.training import (
     one_logger_utils,
 )
 from megatron.training.arguments import (
-    core_transformer_config_from_args,
     moe_freq_type,
     validate_args,
 )
@@ -142,9 +132,10 @@ from megatron.training.utils import (
     update_use_dist_ckpt,
 )
 from megatron.training.yaml_arguments import (
-    core_transformer_config_from_yaml,
     validate_yaml,
 )
+
+from pretrain_gpt import model_provider
 
 from primus.backends.megatron.training.tokenizer.tokenizer import build_tokenizer
 from primus.core.utils import checker, file_utils
@@ -792,7 +783,7 @@ class MegatronTrainer(BaseTrainer, BaseModule):
         self.app_metrics["app_build_optimizer_start_time"] = one_logger_utils.get_timestamp_in_ms()
         log_rank_0(f"-setup_model_and_optimizer...")
         self.model, self.optimizer, self.opt_param_scheduler = self.setup_model_and_optimizer(
-            self.model_provider,
+            model_provider,
             ModelType.encoder_or_decoder,
             checkpointing_context=self.checkpointing_context,
         )
@@ -899,129 +890,6 @@ class MegatronTrainer(BaseTrainer, BaseModule):
         log_rank_0("> finished creating GPT datasets ...")
 
         return train_ds, valid_ds, test_ds
-
-    def model_provider(
-        self, pre_process=True, post_process=True
-    ) -> Union[GPTModel, megatron.legacy.model.GPTModel]:
-        """Builds the model.
-
-        If you set the use_legacy_models to True, it will return the legacy GPT model and if not the mcore GPT model.
-
-        Args:
-            pre_process (bool, optional): Set to true if you need to compute embedings. Defaults to True.
-            post_process (bool, optional): Set to true if you need to want to compute output logits/loss. Defaults to True.
-
-
-        Returns:
-            Union[GPTModel, megatron.legacy.model.GPTModel]: The returned model
-        """
-        args = get_args()
-        use_te = args.transformer_impl == "transformer_engine"
-        checker.check_true(f"Primus only supports the TE transformer impl.")
-
-        if args.record_memory_history:
-            torch.cuda.memory._record_memory_history(
-                True,
-                # keep 100,000 alloc/free events from before the snapshot
-                trace_alloc_max_entries=100000,
-                # record stack information for the trace events
-                trace_alloc_record_context=True,
-            )
-
-            def oom_observer(device, alloc, device_alloc, device_free):
-                # snapshot right after an OOM happened
-                print("saving allocated state during OOM")
-                snapshot = torch.cuda.memory._snapshot()
-                from pickle import dump
-
-                dump(
-                    snapshot,
-                    open(f"oom_rank-{torch.distributed.get_rank()}_{args.memory_snapshot_path}", "wb"),
-                )
-
-            torch._C._cuda_attach_out_of_memory_observer(oom_observer)
-
-        log_rank_0("building GPT model ...")
-        checker.check_true(f"Primus doesn't support megatron yaml config.")
-        # Experimental loading arguments from yaml
-        if args.yaml_cfg is not None:
-            config = core_transformer_config_from_yaml(args, "language_model")
-        else:
-            config = core_transformer_config_from_args(args)
-
-        checker.check_true(f"Primus doesn't support legacy models.")
-        if args.use_legacy_models:
-            model = megatron.legacy.model.GPTModel(
-                config,
-                num_tokentypes=0,
-                parallel_output=True,
-                pre_process=pre_process,
-                post_process=post_process,
-            )
-        else:  # using core models
-            checker.check_true(f"Primus doesn't support spec from args.")
-            log_rank_0("-get transformer layer spec...")
-            if args.spec is not None:
-                transformer_layer_spec = import_module(args.spec)
-            else:
-                if args.num_experts:
-                    # Define the decoder block spec
-                    transformer_layer_spec = get_gpt_decoder_block_spec(config, use_transformer_engine=use_te)
-                else:
-                    # Define the decoder layer spec
-                    if use_te:
-                        transformer_layer_spec = get_gpt_layer_with_transformer_engine_spec(
-                            args.num_experts,
-                            args.moe_grouped_gemm,
-                            args.qk_layernorm,
-                            args.multi_latent_attention,
-                            args.moe_use_legacy_grouped_gemm,
-                        )
-                    else:
-                        transformer_layer_spec = get_gpt_layer_local_spec(
-                            args.num_experts,
-                            args.moe_grouped_gemm,
-                            args.qk_layernorm,
-                            args.multi_latent_attention,
-                            args.moe_use_legacy_grouped_gemm,
-                        )
-
-            build_model_context = nullcontext
-            build_model_context_args = {}
-            if args.fp8_param_gather:
-                try:
-                    from transformer_engine.pytorch import fp8_model_init
-
-                    build_model_context = fp8_model_init
-                    build_model_context_args["enabled"] = True
-
-                    # Check if fp8_model_init supports preserve_high_precision_init_val
-                    if "preserve_high_precision_init_val" in inspect.signature(fp8_model_init).parameters:
-                        build_model_context_args["preserve_high_precision_init_val"] = True
-                except:
-                    raise RuntimeError(
-                        "--fp8-param-gather requires `fp8_model_init` from TransformerEngine, but not found."
-                    )
-
-            log_rank_0("-create GPTModel...")
-            with build_model_context(**build_model_context_args):
-                model = GPTModel(
-                    config=config,
-                    transformer_layer_spec=transformer_layer_spec,
-                    vocab_size=args.padded_vocab_size,
-                    max_sequence_length=args.max_position_embeddings,
-                    pre_process=pre_process,
-                    post_process=post_process,
-                    fp16_lm_cross_entropy=args.fp16_lm_cross_entropy,
-                    parallel_output=True,
-                    share_embeddings_and_output_weights=not args.untie_embeddings_and_output_weights,
-                    position_embedding_type=args.position_embedding_type,
-                    rotary_percent=args.rotary_percent,
-                    rotary_base=args.rotary_base,
-                    rope_scaling=args.use_rope_scaling,
-                )
-
-        return model
 
     def initialize_megatron(
         self,
