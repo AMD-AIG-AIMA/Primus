1f16d59fa66960769cb875e039a32e17c41db469
Xiaoming-AMD <xiaoming.peng@amd.com>
2025-06-11 05:13:18 +0000

    examples: add license header to run_k8s_pretrain.sh

--- examples/run_k8s_pretrain.sh
+++ examples/run_k8s_pretrain.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+###############################################################################
+# Copyright (c) 2025, Advanced Micro Devices, Inc. All rights reserved.
+#
+# See LICENSE for license information.
+###############################################################################
 
 API_URL=""
 CMD=""
