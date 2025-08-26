c1d46c8452b8353f50cc22f034f94b70d65a672d
Ruibin.Cheung <ruibzhan@amd.com>
2025-08-26 09:12:59 +0000

    update script

--- examples/run_pretrain.sh
+++ examples/run_pretrain.sh
@@ -209,7 +209,7 @@ pip install "${PRIMUS_PATH}"/third_party/transformer_engine-1.13.0+d4d1e81c-cp31
 # install primus_turbo
 pip uninstall primus_turbo -y
 pip install -qq hip-python --extra-index-url https://test.pypi.org/simple
-pip install -qq "${PRIMUS_PATH}"/third_party/primus_turbo-0.0.0-cp310-cp310-linux_x86_64.whl
+pip install -qq "${PRIMUS_PATH}"/third_party/primus_turbo-0.0.0+c4d4a3b-cp310-cp310-linux_x86_64.whl
 pip install triton==3.3.0
 
 # Note: Disable v3 due to accuracy issues. Will fix after TE version 2.1.
