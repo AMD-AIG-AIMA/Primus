bfdc8d0217662795bba64cb9c8137e828a1d12c8
lihuan <lihuzhan@amd.com>
2025-09-12 11:09:56 +0800

    disable dump_pp_data when pp size is one

--- primus/modules/trainer/megatron/utils.py
+++ primus/modules/trainer/megatron/utils.py
@@ -369,10 +369,15 @@ def validate_args_on_rocm(args):
         assert (
             args.moe_token_dispatcher_type in support_token_dispatcher_types
         ), f"The token dispatcher type should be {support_token_dispatcher_types}."
-    
+
     # sync_free moe
     if args.use_turbo_sync_free_moe:
         assert args.use_turbo_deepep
         assert args.use_turbo_grouped_mlp
         assert args.moe_permute_fusion
         assert args.expert_model_parallel_size <= 8
+
+    # dump pp data
+    if args.dump_pp_data and args.pipeline_model_parallel_size == 1:
+        args.dump_pp_data = False
+        print_rank_last(f"Disable args.dump_pp_data since args.pipeline_model_parallel_size=1")
